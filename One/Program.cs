e5d596418dc2f5ba25a5d00e2dfdaba098f94086
Diego Rocha <dlmrocha87@gmail.com>
2022-03-22 16:42:11 -0300

    day one

---
+++ One/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+// See https://aka.ms/new-console-template for more information
+var inputs = File.ReadAllLines("input.txt").ToList().ConvertAll(x => Int32.Parse(x));
+var previous = inputs[0];
+inputs.RemoveAt(0);
+var counter = 0;
+
+foreach (var input in inputs)
+{
+    if (input > previous)
+    {
+        counter += 1;
+    }
+    previous = input;
+}
+
+Console.WriteLine(counter);
