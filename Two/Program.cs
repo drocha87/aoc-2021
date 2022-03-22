85ce4641ac2deb23d277bf7ed663c83e7f220abd
Diego Rocha <dlmrocha87@gmail.com>
2022-03-22 16:55:45 -0300

    day two

---
+++ Two/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+var inputs = File.ReadAllLines("input.txt").ToList().ConvertAll(x => Int32.Parse(x));
+var previous = inputs[0] + inputs[1] + inputs[2];
+var counter = 0;
+
+for (var index = 1; index < (inputs.Count() - 2); index++)
+{
+    var sum = inputs[index] + inputs[index + 1] + inputs[index + 2];
+    if (sum > previous)
+    {
+        counter += 1;
+    }
+    previous = sum;
+}
+
+Console.WriteLine(counter);
